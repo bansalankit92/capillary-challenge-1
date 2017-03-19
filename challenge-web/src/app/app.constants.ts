19ff5a1e7527045c661f891c078e126835446895
abansal <abansal.shutterfly.com>
2017-03-19 20:34:17 +0530

    heroku changes

--- challenge-web/src/app/app.constants.ts
+++ challenge-web/src/app/app.constants.ts
@@ -2,6 +2,6 @@
 export  class Constants{
     public static readonly PAGE_NUMBER=1;
     //public static readonly HOST="http://localhost:3000";
-    public static readonly HOST="https://capillary-game-chalenge.herokuapp.com/";
+    public static readonly HOST="https://capillary-game-chalenge.herokuapp.com";
     public static readonly GAMES_URL="/games";
 }
